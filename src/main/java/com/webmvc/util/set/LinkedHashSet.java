81c18d7a16a90dd08f3f78d584ccec9326d3ebed
sgz <179826407@qq.com>
2018-03-09 16:41:02 +0800

    开始set

---
+++ src/main/java/com/webmvc/util/set/LinkedHashSet.java
@@ -0,0 +1,10 @@
+package com.webmvc.util.set;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class LinkedHashSet<E> extends HashSet<E> implements Set<E>, Cloneable, Serializable {
+
+	private static final long serialVersionUID = -2287895743147324610L;
+
+}
